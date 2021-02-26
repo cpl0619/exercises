da5aa3836b6b9d83d8c20692472a84c431ed9557
Christopher Loo <chrisloo0619@gmail.com>
2021-02-26 00:28:08 -0800

    this is to test modify

--- projects/firstproj/helloworld.cpp
+++ projects/firstproj/helloworld.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 int main() {
-std::cout << "Hello World";	
-// l = left, h = right, k = up, j= down, i = insert in line, o = insert next line.
-// shift + : then w  = save project
-// shift + : then q = quit
+	std::cout << "Hello World" << std::endl;
+	// l = left, h = right, k = up, j= down, i = insert in line, o = insert next line.
+	// shift + : then w  = save project
+	// shift + : then q = quit
+        std::cout << "code change exercise" << std::endl;
 
 }
 
