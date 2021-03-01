df411634727904fd785f7d4188175f32b572a088
Christopher Loo <chrisloo0619@gmail.com>
2021-02-28 22:19:29 -0800

    this is a exercise for make files

--- projects/firstproj/helloworld.cpp
+++ projects/firstproj/helloworld.cpp
@@ -4,7 +4,68 @@ int main() {
 	// l = left, h = right, k = up, j= down, i = insert in line, o = insert next line.
 	// shift + : then w  = save project
 	// shift + : then q = quit
+	// esc + u = undo, dd = delete current line, yy = copy current line, p = paste,
         std::cout << "code change exercise" << std::endl;
 
+	// l = left, h = right, k = up, j= down, i = insert in line, o = insert next line.
+	// l = left, h = right, k = up, j= down, i = insert in line, o = insert next line.
+	// l = left, h = right, k = up, j= down, i = insert in line, o = insert next line.
+	// l = left, h = right, k = up, j= down, i = insert in line, o = insert next line.
+	// l = left, h = right, k = up, j= down, i = insert in line, o = insert next line.
+	// l = left, h = right, k = up, j= down, i = insert in line, o = insert next line.
+	// l = left, h = right, k = up, j= down, i = insert in line, o = insert next line.
+	// l = left, h = right, k = up, j= down, i = insert in line, o = insert next line.
+	// l = left, h = right, k = up, j= down, i = insert in line, o = insert next line.
+	// l = left, h = right, k = up, j= down, i = insert in line, o = insert next line.
+	// l = left, h = right, k = up, j= down, i = insert in line, o = insert next line.
+	// l = left, h = right, k = up, j= down, i = insert in line, o = insert next line.
+	// l = left, h = right, k = up, j= down, i = insert in line, o = insert next line.
+	// l = left, h = right, k = up, j= down, i = insert in line, o = insert next line.
+	// shift + : then w  = save project
+	// shift + : then q = quit
+	// esc + u = undo, dd = delete current line, yy = copy current line, p = paste,
+	// shift + : then w  = save project
+	// shift + : then q = quit
+	// esc + u = undo, dd = delete current line, yy = copy current line, p = paste,
+	// shift + : then w  = save project
+	// shift + : then q = quit
+	// esc + u = undo, dd = delete current line, yy = copy current line, p = paste,
+	// shift + : then w  = save project
+	// shift + : then q = quit
+	// esc + u = undo, dd = delete current line, yy = copy current line, p = paste,
+	// shift + : then w  = save project
+	// shift + : then q = quit
+	// esc + u = undo, dd = delete current line, yy = copy current line, p = paste,
+	// shift + : then w  = save project
+	// shift + : then q = quit
+	// esc + u = undo, dd = delete current line, yy = copy current line, p = paste,
+	// shift + : then w  = save project
+	// shift + : then q = quit
+	// esc + u = undo, dd = delete current line, yy = copy current line, p = paste,
+	// shift + : then w  = save project
+	// shift + : then q = quit
+	// esc + u = undo, dd = delete current line, yy = copy current line, p = paste,
+	// shift + : then w  = save project
+	// shift + : then q = quit
+	// esc + u = undo, dd = delete current line, yy = copy current line, p = paste,
+	// shift + : then w  = save project
+	// shift + : then q = quit
+	// esc + u = undo, dd = delete current line, yy = copy current line, p = paste,
+	// shift + : then w  = save project
+	// shift + : then q = quit
+	// esc + u = undo, dd = delete current line, yy = copy current line, p = paste,
+	// shift + : then w  = save project
+	// shift + : then q = quit
+	// esc + u = undo, dd = delete current line, yy = copy current line, p = paste,
+	// shift + : then w  = save project
+	// shift + : then q = quit
+	// esc + u = undo, dd = delete current line, yy = copy current line, p = paste,
+	// shift + : then w  = save project
+	// shift + : then q = quit
+	// esc + u = undo, dd = delete current line, yy = copy current line, p = paste,
 }
 
+	// l = left, h = right, k = up, j= down, i = insert in line, o = insert next line.
+	// shift + : then w  = save project
+	// shift + : then q = quit
+	// esc + u = undo, dd = delete current line, yy = copy current line, p = paste,
